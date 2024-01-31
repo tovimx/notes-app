722d2270a15d5ab0ffbee1f645060c3d2bca654f
Tovias Torres <tovias.torres@herodigital.com>
2024-01-31 13:51:38 -0600

    build: add script to skip tsc

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { NotesList } from "./NotesList";
 import { NoteForm } from "./NoteForm";
 
