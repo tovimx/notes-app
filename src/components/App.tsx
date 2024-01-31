b8bc740879c7c873d72360ea158741a14328300e
Tovias Torres <tovias.torres@herodigital.com>
2024-01-31 13:53:24 -0600

    importing react

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { NotesList } from "./NotesList";
 import { NoteForm } from "./NoteForm";
 
